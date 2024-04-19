d03ab23cf33b8831ec4ef4fc572f0f6519639de0
Oleksandra <oleksandra.rutkovska@gmailcom>
2024-04-19 15:26:52 +0300

    upd 4.1

--- hw/hw04/OleksandraHospodinina/HW04.py
+++ hw/hw04/OleksandraHospodinina/HW04.py
@@ -16,3 +16,15 @@ if num % 2 == 0:
     print(f"Number {num} is even number")
 else:
     print(f"Number {num} is odd number")
+
+#4.1
+c = float(input("Enter a temperature in Celsius: "))
+a = float(0 - 273.15)
+
+if c >= a:
+    f = int(c * 9 / 5 + 54)
+    print(f"Enter the temperature in Celsius: {c}")
+    print(f" {c}°C is equivalent to {f}°F")
+else:
+    print(f"Enter the temperature in Celsius: {c}")
+    print("Error: Temperature below absolute zero (-273.15°C)")
