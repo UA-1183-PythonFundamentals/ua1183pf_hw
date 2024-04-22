d3508b468d37373d3c12d2aff084eeab29fd8616
IgorKurilov <45769419+IgorKurilov@users.noreply.github.com>
2024-04-22 12:26:02 +0300

    Create Task3_3.py
    Add Task3_3.py

---
+++ hw/hw03/IgorKurilov/Task3_3.py
@@ -0,0 +1,11 @@
+# Initial values
+a = 5
+b = 10
+
+# Interchange values without using a third variable
+a = a + b
+b = a - b
+a = a - b
+
+print("Value of a after interchange:", a)
+print("Value of b after interchange:", b)
