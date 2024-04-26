be01f85cd8e14def87f07285e5d40720c9a22ef5
Mykyta Paiuka <nikvitka.it@gmail.com>
2024-04-26 14:28:19 +0300

    add solution for 2 task

---
+++ hw/hw06/nikvitkaua/task2.py
@@ -0,0 +1,7 @@
+# Task 2
+user_login = input("Hello, buddy! Enter your login: ")
+
+while user_login != "First":
+    user_login = input("I don't know user with this login, try again: ")
+
+print('Welcome home, honey ^^')
