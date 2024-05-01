622acd6cb506e796b6da51e6a080c98c620cbc48
Mykyta Paiuka <nikvitka.it@gmail.com>
2024-05-01 10:25:52 +0300

    done 1t task

--- hw/hw07/nikvitkaua/task1.py
+++ hw/hw07/nikvitkaua/task1.py
@@ -1,1 +1,10 @@
 # Task1
+def get_largest_num(a, b):
+    """
+    Function return largest number
+
+    :param a: number type float or int
+    :param b: number type float or int
+    :return: largest number
+    """
+    return a if a > b else b
