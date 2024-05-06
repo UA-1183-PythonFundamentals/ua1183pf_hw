d58ca2373f5967e4ab513ae30f1ad75d3b0ac429
Liubomyr Halamaha <galamagal86@gmail.com>
2024-05-06 18:55:47 +0300

    added lesson08

---
+++ lessons/lesson08/first.py
@@ -0,0 +1,40 @@
+# import sys
+# from pprint import pprint
+# sys.path.append("c:\\data\\github\\ua1183pf_hw")
+# from hw.hw03.IgorKurilov.Task3_1 import philosophy
+# import hw.hw03.IgorKurilov.Task3_1
+# pprint(sys.path)
+# print(philosophy)
+# print(hw.hw03.IgorKurilov.Task3_1.philosophy)
+
+# import hw.hw03.IgorKurilov.Task3_1 as pp
+
+# print(pp.philosophy)
+# l=[1,2,3,4,5,6]
+# print(sum(l))
+# from second import sum as my_sum, PI, E as ee
+
+# print(sum(l))
+# print(my_sum(*l))
+
+# import math
+# from math import *
+# print(dir())
+# print(math.sin(10))
+# print(sin(10))
+
+# import fibo
+# from inner.fibo2 import *
+
+# fibo.fib(10)
+
+# print(fibo.fib2(10))
+
+# print(f"{__name__=}")
+# from inner import Fibo
+
+# from inner.temp import a, _a, __a
+
+# print(a, _a, __a)
+from inner.temp import *
+print(dir())
