4bdd8a572477314d9c9d22cb3033d56ec716c68c
Oleksandra <oleksandra.rutkovska@gmailcom>
2024-05-08 19:24:36 +0300

    Task 3

---
+++ hw/hw08/OleksandraHospodinina/Task 3/calculate.py
@@ -0,0 +1,10 @@
+from math import pi, pow
+
+def rectangle_area(a, b):
+    return a * b
+
+def triangle_area(h, a):
+    return 0.5 * h * a
+
+def circle_area(r):
+    return math.pi * math.pow(r, 2)
