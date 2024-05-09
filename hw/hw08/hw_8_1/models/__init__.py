1487fedd771679994813c01f27bec5914309f75d
Alex Bodnar <bodnaro926@gmail.com>
2024-05-09 16:14:18 +0300

    added task 8.1

---
+++ hw/hw08/hw_8_1/models/__init__.py
@@ -0,0 +1,5 @@
+
+from .admin import create_admin
+from .user import create_user
+
+__all__ = ['create_admin', 'create_user']
---
+++ hw/hw08/hw_8_1/utils/__init__.py
@@ -0,0 +1,7 @@
+
+from .logger import log_in_file
+from .formatter import format_string
+
+__all__ = ['log_in_file', 'format_string']
+
+print(__all__)
