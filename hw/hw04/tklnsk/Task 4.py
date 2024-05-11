c3f258834ab33043c83902cf201db0564b3f93fd
Tetiana Kalinovska <kalinovskatetiana@gmail.com>
2024-05-11 14:13:06 +0200

    Task 4

---
+++ hw/hw04/tklnsk/Task 4.py
@@ -0,0 +1,9 @@
+
+celsius_conv = float(input("Enter the temperature in Celsius: "))
+if celsius_conv < -273.15:
+    print("Error: Temperature below absolute zero (-273.15°C), try again")
+else:
+    fahrenheit = (celsius_conv * 9 / 5) + 32
+    print(f"The {celsius_conv}°C equals to {fahrenheit}°F")
+
+
