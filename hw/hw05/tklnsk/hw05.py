138e852516dc9b73526f258b9c81ae30ca187b67
Tetiana Kalinovska <kalinovskatetiana@gmail.com>
2024-05-12 19:50:04 +0200

    HW05

---
+++ hw/hw05/tklnsk/hw05.py
@@ -0,0 +1,25 @@
+#Task 1
+integer_list = [1, 10, 15, 20, 25]
+float_list = []
+for num in integer_list:
+    float_list.append(float(num))
+print(float_list)
+
+#Task 2
+x = int(input("Please, enter a number: "))
+a, b = 0, 1
+fibon_sequence = []
+while a <= x:
+    fibon_sequence.append(a)
+    a, b = b, a + b
+print(f"Fibonacci numbers up to {x}:")
+print(fibon_sequence)
+
+#Task 3
+n = int(input("Enter your number: "))
+print(range(1, n))
+factorial = 1
+for i in range(1, n+1):
+    factorial *= i
+
+print('Factorial result :', factorial)
