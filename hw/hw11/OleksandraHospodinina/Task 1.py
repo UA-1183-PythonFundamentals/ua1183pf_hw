cc39077fca0e37287b89517e0c75f81bb4355fe9
Oleksandra <oleksandra.rutkovska@gmailcom>
2024-05-15 21:23:05 +0300

    task 2 and upd task 1

--- hw/hw11/OleksandraHospodinina/Task 1.py
+++ hw/hw11/OleksandraHospodinina/Task 1.py
@@ -1,14 +1,14 @@
 class UserException(Exception):
     pass
 
-
 class User:
     def __init__(self, age:int):
         self.age = age
+
     def input_age(self):
-        #if type(self.age) is not int:
-            #raise UserException("Age must be an integer")
-        if self.age < 0:
+        if not isinstance(self.age, int):
+            raise UserException("Age must be an integer")
+        if self.age <= 0:
             raise UserException("Age must be a positive number")
         elif self.age % 2 == 0:
             print("Even")
@@ -18,6 +18,8 @@ try:
     age = int(input("Enter your age: "))
     age_input = User(age)
     result = age_input.input_age()
-except UserException as e:
-    print(e)
+except ValueError as e:
+    print("Age must be an integer")
+except UserException as er:
+    print(er)
 
