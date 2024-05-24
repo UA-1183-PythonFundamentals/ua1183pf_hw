ac8eee88ebcd89f592ecf2fb6ec5b54d03e6ca13
IgorKurilov <45769419+IgorKurilov@users.noreply.github.com>
2024-05-24 14:50:53 +0300

    Update pin_pong_game.py

--- projects/Igor Kurilov/pin_pong_game.py
+++ projects/Igor Kurilov/pin_pong_game.py
@@ -97,7 +97,9 @@ def show_titles():
     titles = [
         "SOFTSERVE ACADEMY PRESENT",
         "PING PONG GAME",
-        "written by Igor Kurilov"
+        "written by Igor Kurilov",
+        "move paddle for player1 is W and S",
+        "move paddle for player2 is up and down arrows"
     ]
     
     for title in titles:
@@ -105,7 +107,7 @@ def show_titles():
         title_text = FONT.render(title, 1, WHITE)
         WIN.blit(title_text, (WIDTH//2 - title_text.get_width()//2, HEIGHT//2 - title_text.get_height()//2))
         pygame.display.update()
-        time.sleep(2)  # Display each title for 2 seconds
+        time.sleep(3)  # Display each title for 3 seconds
 
 def main():
     global player1_score, player2_score
